d6621a1ce9c17e98104e875bd604070401b43ae7
ahmad14022 <ahmad14022@users.noreply.github.com>
2024-03-07 04:30:20 +0000

    deploy: 68f727b3f6d1864e0b0601f382132bd14c8c1202

---
+++ assets/AuthenticatedView-YwU8ow6o.js
@@ -0,0 +1,1 @@
+import{_ as t,c as a,a as r,t as s,o as c}from"./index-7fAaQ31s.js";const n={};function o(e,i){return c(),a("div",null,[r("h1",null,s(e.$route.params.id||"User")+" Detail",1)])}const _=t(n,[["render",o]]);export{_ as default};
---
+++ assets/NotFoundView-qqkNYpQh.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./index-7fAaQ31s.js";const n={};function t(r,a){return c(),o("h1",null,"Not Found Page")}const _=e(n,[["render",t]]);export{_ as default};
