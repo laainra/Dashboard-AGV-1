4b422aa85c9e87356722b7a41756584e6be70b62
Jessicasgc <jessicaamadearahma@gmail.com>
2024-04-05 13:20:45 +0700

    Try to use ros 2 and ros 2d

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import ToastPlugin from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-bootstrap.css';
-import ROSLIB from 'roslib/src/RosLib'
 
 const appInstance = createApp(App);
 appInstance.use(createPinia());
@@ -23,34 +22,3 @@ appInstance.use(ArgonDashboard);
 appInstance.mount("#app");
 appInstance.use(Toast, { timeout: 1500 });
 appInstance.use(ToastPlugin);
-
-// Create ros object to communicate over your Rosbridge connection
-const ros = new ROSLIB.Ros({ url: "ws://localhost:9090" });
-
-// When the Rosbridge server connects, fill the span with id "status" with "successful"
-ros.on("connection", () => {
-    console.log("Connection");
-});
-
-// When the Rosbridge server experiences an error, fill the "status" span with the returned error
-ros.on("error", (error) => {
-    console.log("Error");
-  
-});
-
-// When the Rosbridge server shuts down, fill the "status" span with "closed"
-ros.on("close", () => {
-    console.log("Close");
-});
-
-// Create a listener for /my_topic
-const my_topic_listener = new ROSLIB.Topic({
-    ros,
-    name: "/my_topic",
-    messageType: "std_msgs/String",
-  });
-  
-  // When we receive a message on /my_topic, add its data as a list item to the "messages" ul
-  my_topic_listener.subscribe((message) => {
-    console.log(message)
-  });
--- src/views/DashboardDefault.vue
+++ src/views/DashboardDefault.vue
@@ -1,130 +1,118 @@
 <template>
   <div class="py-4 container-fluid">
     <div class="row">
-      <div class="col-lg-12">
-        <div class="row">
-          <div class="col-lg-3 col-md-6 col-6">
-            <card
-              :title="stats.money.title"
-              :value="stats.money.value"
-              :percentage="stats.money.percentage"
-              :iconClass="stats.money.iconClass"
-              :iconBackground="stats.money.iconBackground"
-              :detail="stats.money.detail"
-              directionReverse
-            ></card>
-          </div>
-          <div class="col-lg-3 col-md-6 col-12">
-            <card
-              :title="stats.users.title"
-              :value="stats.users.value"
-              :percentage="stats.users.percentage"
-              :iconClass="stats.users.iconClass"
-              :iconBackground="stats.users.iconBackground"
-              :detail="stats.users.detail"
-              directionReverse
-            ></card>
-          </div>
-          <div class="col-lg-3 col-md-6 col-12">
-            <card
-              :title="stats.clients.title"
-              :value="stats.clients.value"
-              :percentage="stats.clients.percentage"
-              :iconClass="stats.clients.iconClass"
-              :iconBackground="stats.clients.iconBackground"
-              :percentageColor="stats.clients.percentageColor"
-              :detail="stats.clients.detail"
-              directionReverse
-            ></card>
-          </div>
-          <div class="col-lg-3 col-md-6 col-12">
-            <card
-              :title="stats.sales.title"
-              :value="stats.sales.value"
-              :percentage="stats.sales.percentage"
-              :iconClass="stats.sales.iconClass"
-              :iconBackground="stats.sales.iconBackground"
-              :detail="stats.sales.detail"
-              directionReverse
-            ></card>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-7 mb-lg">
-            <!-- line chart -->
-            <div class="card z-index-2">
-              <gradient-line-chart />
+      <div class="d-flex flex-column justify-content-center align-items-center gap-4">
+
+
+        <div class="card mb-4">
+          <div id="map" class="d-flex flex-column justify-content-center align-items-center
+          sm:h-auto md:h-500" :class="navigationModeClass">
+            <div class="p-3 w-100">
+              <button class=" btn-danger btn-lg w-100" v-if="connected === false" @click="init">Connect GUI To
+                Robot</button>
             </div>
+            <img v-if="!robotConnected" src="src/assets/img/robot-with-pliers.png" style="width: 30%;" />
           </div>
-          <div class="col-lg-5">
-            <carousel />
-          </div>
-        </div>
-        <div class="row mt-4">
-          <div class="col-lg-12 mb-lg-0 mb-4">
-            <div class="card">
-              <div class="p-3 pb-0 card-header">
-                <div class="d-flex justify-content-between">
-                  <h6 class="mb-2">Data Station AGV Lidar</h6>
-                  <router-link to="/tables" class="text-end"
-                    >See details</router-link
-                  >
-                </div>
-              </div>
-              <div class="table-responsive">
-                <table class="table align-items-center">
-                  <tbody>
-                    <tr v-for="(sale, index) in sales" :key="index">
-                      <td class="w-30">
-                        <div class="px-2 py-1 d-flex align-items-center">
-                          <div>
-                            <img :src="sale.flag" alt="Country flag" />
-                          </div>
-                          <div class="ms-4">
-                            <p class="mb-0 text-xs font-weight-bold">
-                              Country:
-                            </p>
-                            <h6 class="mb-0 text-sm">{{ sale.country }}</h6>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="text-center">
-                          <p class="mb-0 text-xs font-weight-bold">Sales:</p>
-                          <h6 class="mb-0 text-sm">{{ sale.sales }}</h6>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="text-center">
-                          <p class="mb-0 text-xs font-weight-bold">Value:</p>
-                          <h6 class="mb-0 text-sm">{{ sale.value }}</h6>
-                        </div>
-                      </td>
-                      <td class="text-sm align-middle">
-                        <div class="text-center col">
-                          <p class="mb-0 text-xs font-weight-bold">Bounce:</p>
-                          <h6 class="mb-0 text-sm">{{ sale.bounce }}</h6>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <!-- <div class="col-lg-5">
-            <categories-card />
-          </div> -->
         </div>
       </div>
+
+      <div class="col-lg-3 col-md-6 col-6">
+        <card :title="stats.money.title" :value="stats.money.value" :percentage="stats.money.percentage"
+          :iconClass="stats.money.iconClass" :iconBackground="stats.money.iconBackground" :detail="stats.money.detail"
+          directionReverse></card>
+      </div>
+      <div class="col-lg-3 col-md-6 col-12">
+        <card :title="stats.users.title" :value="stats.users.value" :percentage="stats.users.percentage"
+          :iconClass="stats.users.iconClass" :iconBackground="stats.users.iconBackground" :detail="stats.users.detail"
+          directionReverse></card>
+      </div>
+      <div class="col-lg-3 col-md-6 col-12">
+        <card :title="stats.clients.title" :value="stats.clients.value" :percentage="stats.clients.percentage"
+          :iconClass="stats.clients.iconClass" :iconBackground="stats.clients.iconBackground"
+          :percentageColor="stats.clients.percentageColor" :detail="stats.clients.detail" directionReverse></card>
+      </div>
+      <div class="col-lg-3 col-md-6 col-12">
+        <card :title="stats.sales.title" :value="stats.sales.value" :percentage="stats.sales.percentage"
+          :iconClass="stats.sales.iconClass" :iconBackground="stats.sales.iconBackground" :detail="stats.sales.detail"
+          directionReverse></card>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-7 mb-lg">
+        <!-- line chart -->
+        <div class="card z-index-2">
+          <gradient-line-chart />
+        </div>
+      </div>
+      <div class="col-lg-5">
+        <carousel />
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col-lg-12 mb-lg-0 mb-4">
+        <div class="card">
+          <div class="p-3 pb-0 card-header">
+            <div class="d-flex justify-content-between">
+              <h6 class="mb-2">Data Station AGV Lidar</h6>
+              <router-link to="/tables" class="text-end">See details</router-link>
+            </div>
+          </div>
+          <div class="table-responsive">
+            <table class="table align-items-center">
+              <tbody>
+                <tr v-for="(sale, index) in sales" :key="index">
+                  <td class="w-30">
+                    <div class="px-2 py-1 d-flex align-items-center">
+                      <div>
+                        <img :src="sale.flag" alt="Country flag" />
+                      </div>
+                      <div class="ms-4">
+                        <p class="mb-0 text-xs font-weight-bold">
+                          Country:
+                        </p>
+                        <h6 class="mb-0 text-sm">{{ sale.country }}</h6>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="text-center">
+                      <p class="mb-0 text-xs font-weight-bold">Sales:</p>
+                      <h6 class="mb-0 text-sm">{{ sale.sales }}</h6>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="text-center">
+                      <p class="mb-0 text-xs font-weight-bold">Value:</p>
+                      <h6 class="mb-0 text-sm">{{ sale.value }}</h6>
+                    </div>
+                  </td>
+                  <td class="text-sm align-middle">
+                    <div class="text-center col">
+                      <p class="mb-0 text-xs font-weight-bold">Bounce:</p>
+                      <h6 class="mb-0 text-sm">{{ sale.bounce }}</h6>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="col-lg-5">
+            <categories-card />
+          </div> -->
     </div>
   </div>
+
+
 </template>
 <script>
 import Card from "@/examples/Cards/Card.vue";
 import GradientLineChart from "@/examples/Charts/GradientLineChart.vue";
 import Carousel from "./components/Carousel.vue";
 import CategoriesCard from "./components/CategoriesCard.vue";
+import ROSLIB from 'roslib';
+
 
 import US from "@/assets/img/icons/flags/US.png";
 import DE from "@/assets/img/icons/flags/DE.png";
@@ -135,7 +123,12 @@ export default {
   name: "dashboard-agv-lidar",
   data() {
     return {
+      ros: null,
+      connected: false,
+      mapViewer: null,
+      mapGridClient: null,
       stats: {
+
         money: {
           title: "Jumlah Station",
           value: "2",
@@ -202,6 +195,40 @@ export default {
       },
     };
   },
+  mounted() {
+    this.ros = new ROSLIB.Ros({
+      url: 'ws://' + window.location.hostname + ':9090',
+    });
+
+    this.ros.on('connection', () => {
+      this.connected = true;
+      console.log('Connection to ROSBridge established');
+    });
+    this.ros.on('error', (error) => {
+      console.log('Something went wrong when trying to connect');
+      console.log(error);
+    });
+    this.ros.on('close', () => {
+      this.connected = false;
+      console.log('Connection to ROSBridge was closed');
+    });
+
+    this.mapViewer = new ROS2D.Viewer({
+      divID: 'map',
+      width: 400,
+      height: 400,
+    });
+
+    this.mapGridClient = new ROS2D.OccupancyGridClient({
+      ros: this.ros,
+      rootObject: this.mapViewer.scene,
+      continuous: true,
+    });
+
+    this.mapGridClient.on('change', () => {
+      this.mapViewer.scaleToDimensions(this.mapGridClient.currentGrid.width, this.mapGridClient.currentGrid.height);
+    });
+  },
   components: {
     Card,
     GradientLineChart,
@@ -210,3 +237,21 @@ export default {
   },
 };
 </script>
+<style>
+.lidar-row {
+  display: flex;
+}
+
+.lidar-cell {
+  width: 10px;
+  height: 10px;
+}
+
+.lidar-cell.obstacle {
+  background-color: red;
+}
+
+.lidar-cell.free {
+  background-color: green;
+}
+</style>
